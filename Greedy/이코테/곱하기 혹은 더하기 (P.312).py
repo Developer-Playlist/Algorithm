b16f124449fb2e16345e2ede4105e2220557456c
GiSeong Ji <89971435+jiggyjiggy@users.noreply.github.com>
2022-05-31 21:43:22 +0900

    Update 곱하기 혹은 더하기 (P.312).py

--- Greedy/이코테/곱하기 혹은 더하기 (P.312).py
+++ Greedy/이코테/곱하기 혹은 더하기 (P.312).py
@@ -12,3 +12,65 @@ for i in s:
 print(result)
 
 ###
+
+###
+# 기성 [O] : 다만 하드코딩 느낌이 강하다 / 모범답안처럼 반복문 속에서 그냥 처리 가능하다 (따로 리스트를 )
+nums = input()
+
+target = []
+for i in range(len(nums)):
+    target.append(nums[i])
+
+result = int(target[0])
+for idx, i in enumerate(target[1:]):
+    idx +=1
+    if nums[idx - 1] == '0':
+        result += int(i)
+    elif nums[idx - 1] == '1':
+        result += 0
+    else:
+        result *= int(i)
+
+print(result)
+###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###
+# 입력예시
+# 02984
+#
+# 출력예시
+# 576
+
+# 모범답안
+data = input()
+
+result = int(data[0])
+
+for i in range(1, len(data)):
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result += num
+
+print(result)
+###
