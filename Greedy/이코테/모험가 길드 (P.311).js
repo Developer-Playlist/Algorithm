6e6a4aae4df76ff4b69da276dec5fa03959c1119
HaYoung Lee <98532217+Rumisister@users.noreply.github.com>
2022-05-31 23:44:32 +0900

    Update 모험가 길드 (P.311).js

--- Greedy/이코테/모험가 길드 (P.311).js
+++ Greedy/이코테/모험가 길드 (P.311).js
@@ -21,21 +21,3 @@ for(let b of arr){
 console.log(group) // 2
 /**/
 
-/**/
-// 2. <하영>[x]
-const mOrP = s => {
-  const numArr = s.split("").map(x => +x);
-  let result = numArr[0];
-  
-  for (let i = 1; i < numArr.length; i++) {
-    if (result <= 1) {
-      result += numArr[i];
-    } else if(numArr[i] <= 1) {
-      result += numArr[i];
-    } else {
-      result *= numArr[i];
-    }
-  }
-  
-  return result;
-};
