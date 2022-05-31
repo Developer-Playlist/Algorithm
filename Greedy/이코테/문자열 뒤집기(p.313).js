061fc26506db7920e333ff3c07f6e1affc914a3c
HaYoung Lee <98532217+Rumisister@users.noreply.github.com>
2022-05-31 23:52:56 +0900

    Create 문자열 뒤집기(p.313).js

---
+++ Greedy/이코테/문자열 뒤집기(p.313).js
@@ -0,0 +1,17 @@
+/**/
+//<하영> [o]
+const result = s =>{
+  let count = 0;
+  for(let i = 1; i < s.length; i++) {
+    if(s[i-1] !== s[i]){
+      count += 1;
+    }
+  }
+  if(count % 2 === 0){
+    return (count / 2)
+  }else if(count % 2 !== 0){
+    return Math.ceil(count / 2)
+  }
+}
+console.log(result("1010101010111")) // 5출력
+/**/
