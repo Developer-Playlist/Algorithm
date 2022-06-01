08a8c663f712667d564312990f670fa1ec0eab08
MinseokChoi <alexms0316@gmail.com>
2022-06-01 14:44:09 +0900

    Update 문자열 뒤집기 (P.313).py

--- Greedy/이코테/문자열 뒤집기 (P.313).py
+++ Greedy/이코테/문자열 뒤집기 (P.313).py
@@ -23,7 +23,18 @@ result = min(zero_group, one_group)
 print(result)
 ###
 
+###
+# <민석> [0]
+S = input()
 
+result = 0
+
+for i in range(1, len(S)):
+  if S[i-1] != S[i]:
+    result += 1
+
+print(result-1)
+###
 
 
 
