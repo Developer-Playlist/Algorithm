3b63dad73556c980e4877e4ba7cbff6325038c22
HaYoung Lee <98532217+Rumisister@users.noreply.github.com>
2022-06-03 01:41:23 +0900

    Update 만들 수 없는 금액 (P.314).js

--- Greedy/이코테/만들 수 없는 금액 (P.314).js
+++ Greedy/이코테/만들 수 없는 금액 (P.314).js
@@ -1,1 +1,20 @@
+/**/
+//<하영>[o] : 하드코딩이 아닌 test case에 따른 입력값을 받아 적용할수있게 해봐야겠다
+coins = [1,2,3,5,16]
 
+const coinArr = coins.sort((a,b) => {
+  return a-b;
+})
+
+let count = 1;
+
+for(let i=0; i < coinArr.length; i++){
+  if(count < coinArr[i]){
+    break
+  }else{
+    count += coinArr[i]
+  }
+}
+
+console.log(count) // 12출력
+/**/
