b435bfa99f83ec6c70b5deb415fd6077b3c3b67d
CHEESECHOUX <89918678+CHEESECHOUX@users.noreply.github.com>
2022-06-07 22:58:56 +0900

    Update 볼링공 고르기 (P.315).py

--- Greedy/이코테/볼링공 고르기 (P.315).py
+++ Greedy/이코테/볼링공 고르기 (P.315).py
@@ -30,7 +30,25 @@ print(result)
 ###
 
 
+###
+# 지수 [x] : 정답보고 간신히.. 이해. A가 선택하면 B가 선택할 수 있는 경우의 수는 줄어든 다는 것을 생각하지 못함. (중복되는 경우의 수는 피해야 하니까)
 
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+weight = [0] * 11 
+
+for x in data:
+    weight[x] += 1
+#공의 무게가 1부터 10까지 있음. 길이가 11인 리스트로 초기화한 후 for문을 돌면서 공이 몇 개 있는지 갯수 세서 저장
+
+result = 0
+for i in range(1, m + 1):
+    n -= weight[i] #무게가 i인 A가 선택할 수 있는 경우는 제외 
+    result += weight[i] * n #B가 선택하는 경우의 수와 곱하기
+
+print(result)
+###
 
 
 
