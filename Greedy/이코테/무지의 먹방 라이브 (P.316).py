59c0bf81fd7c7e9a4b87babd880db3ad0360a2d4
celeste-6 <96307607+celeste-6@users.noreply.github.com>
2022-06-09 00:33:19 +0900

    Update 무지의 먹방 라이브 (P.316).py

--- Greedy/이코테/무지의 먹방 라이브 (P.316).py
+++ Greedy/이코테/무지의 먹방 라이브 (P.316).py
@@ -47,3 +47,26 @@ def solution(food_times, k):
 
     return answer
 ###
+
+# <현지> [X] : 어디서부터 잘못된지 모르겠어서 답지 봄..
+import heapq
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1 
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i))
+    used = 0
+    previous = 0
+    remain = len(food_times)
+    while (used + ((q[0][0] - previous) * remain) <= k):
+        now = heapq.heappop(q)[0]
+        used = used + (now - previous) * remain
+        remain = remain - 1
+        previous = now
+    answer = sorted(q, key=lambda x: x[1])
+    return answer[(k - used) % remain][1]
+###
+
+
