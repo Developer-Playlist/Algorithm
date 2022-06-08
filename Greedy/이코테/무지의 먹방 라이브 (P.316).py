6f30f092d904fb740376b78e96c284978e2c52b0
GiSeong Ji <89971435+jiggyjiggy@users.noreply.github.com>
2022-06-09 00:15:18 +0900

    Update 무지의 먹방 라이브 (P.316).py

--- Greedy/이코테/무지의 먹방 라이브 (P.316).py
+++ Greedy/이코테/무지의 먹방 라이브 (P.316).py
@@ -1,5 +1,5 @@
 ###
-# 기성 [X] : ㅋㅋ,, 하,,
+# <기성> [X] : ㅋㅋ,, 하,,
 def solution(food_times, k):
     iter = sum(food_times)
     length =  len(food_times)
@@ -22,4 +22,28 @@ def solution(food_times, k):
     return answer
 
 print(solution([3, 1, 2], 5))
+
+# <기성 2> [X] : 2/100 -> 런타임 에러 -> 그리디로 풀어야함
+def solution(food_times, k):
+    iter = sum(food_times)
+    length = len(food_times)
+    zero_count = 0
+    limit = 0
+    if iter >= k:
+        for i in range(k+1):
+            if i >= length:
+                loop = length // i
+                if loop == 1:
+                    limit += 1
+            loop_idx = i - length * limit
+            if food_times[loop_idx] == 0:
+                zero_count += 1
+                pass
+            else:
+                food_times[loop_idx] -= 1
+        answer = loop_idx + zero_count + 1 - length * limit
+    else:
+        answer = -1
+
+    return answer
 ###
