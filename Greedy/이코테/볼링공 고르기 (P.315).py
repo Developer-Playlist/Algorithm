fd901a6fece69f4f88be42fc3321fec7bdb84fac
MinseokChoi <alexms0316@gmail.com>
2022-06-09 00:20:57 +0900

    Update 볼링공 고르기 (P.315).py

--- Greedy/이코테/볼링공 고르기 (P.315).py
+++ Greedy/이코테/볼링공 고르기 (P.315).py
@@ -50,18 +50,22 @@ for i in range(1, m + 1):
 print(result)
 ###
 
+###
+# 민석 [x] : 벌금냈음ㅜ
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
 
+array = [0] * 11
 
+for x in data:
+    array[x] += 1
 
+result = 0
+for i in range(1, m + 1):
+    n -= array[i]
+    result += array[i] * n
 
-
-
-
-
-
-
-
-
+print(result)
 
 
 
