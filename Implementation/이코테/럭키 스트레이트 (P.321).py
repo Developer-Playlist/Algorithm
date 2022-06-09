ad1be111ff8397d4288829162e4b82bc881027fe
GiSeong Ji <89971435+jiggyjiggy@users.noreply.github.com>
2022-06-09 22:33:54 +0900

    Update 럭키 스트레이트 (P.321).py

--- Implementation/이코테/럭키 스트레이트 (P.321).py
+++ Implementation/이코테/럭키 스트레이트 (P.321).py
@@ -1,1 +1,56 @@
+###
+# <기성> [O] : 매우 간단하지만 모범답안 보면 그 와중에 메모리 적게 쓰려고 하는게 보임
+data = input()
 
+length = len(data)
+
+left_target = 0
+right_target = 0
+
+for left in data[:(length // 2)]:
+    left_target += int(left)
+for right in data[(length // 2):]:
+    right_target += int(right)
+
+if left_target == right_target:
+    print("LUCKY")
+else:
+    print("READY")
+###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###
+# 입력예시
+# 123402
+#
+# 출력예시
+# LUCKY
+
+# 모범답안
+n = input()
+length = len(n)
+summary = 0
+
+for i in range(length // 2):
+    summary += int(n[i])
+
+for i in range(length // 2, length):
+    summary -= int(n[i])
+###
