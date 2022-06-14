3f2685d6016e176c98289885c46c8ed00f88793a
HaYoung Lee <98532217+Rumisister@users.noreply.github.com>
2022-06-14 23:30:39 +0900

    Create 자물쇠와 열쇠 (P.325).js

---
+++ Implementation/이코테/자물쇠와 열쇠 (P.325).js
@@ -0,0 +1,7 @@
+/**/
+// <하영> [x] : 구현불가.. key를 n-1만큼 앞뒤로 늘려서 0으로 다 채운다음에 4번의 회전을 해서 가능한 모든 경우에 대해 key를 만들면 될까.. 
+function solution(key, lock) {
+    var answer = true;
+    return answer;
+}
+/**/
