e1df45e70b6d410443da802931c5f43de1c7444a
celeste-6 <96307607+celeste-6@users.noreply.github.com>
2022-06-27 22:44:58 +0900

    Update 큰 수의 법칙 (P.92).py

--- Greedy/이코테/큰 수의 법칙 (P.92).py
+++ Greedy/이코테/큰 수의 법칙 (P.92).py
@@ -32,3 +32,37 @@ print(result)
 
 ###
 
+###
+# <현지> [X]
+
+# 생각했던 로직
+# 큰 숫자를 저장해서 사용해야한다는 생각을 못했다
+
+# N, M, K를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
+
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort() # 입력받은 수 정렬 
+first = data[n - 1] # 가장 큰 수
+second = data[n - 2] # 두 번째로 가장 큰 수 
+
+result = 0
+
+while True:
+  for i in range(k):
+    if m == 0: # m이 0이라면 반복문 탈출
+      break
+    result += first # 가장 큰 수 한 번 더하기
+    m -= 1 # 더할 때마다 m에서 1씩 빼기
+  if m == 0:
+    break
+  result += second # 두 번째로 가장 큰 수 한 번 더하기
+  m -= 1 # 더할 때마다 m에서 1씩 빼기 
+  
+print(result)
+
+
+###
+
